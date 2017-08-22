371e525dec838fef46ed7be5735dad263e4b978d
chen <chen470@gmail.com>
2017-08-22 16:41:37 +0300

    Added fragments and methods in their interface

--- app/src/main/java/com/cambio/finalprojectandroid/MainActivity.java
+++ app/src/main/java/com/cambio/finalprojectandroid/MainActivity.java
@@ -3,11 +3,42 @@ package com.cambio.finalprojectandroid;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements EventListFragment.OnFragmentInteractionListener, EventEditFragment.OnFragmentInteractionListener, EventAddFragment.OnFragmentInteractionListener, EventDetailsFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onEventClickInteraction(String eventID) {
+
+    }
+
+    @Override
+    public void onSaveEventInteraction() {
+
+    }
+
+    @Override
+    public void onDeleteEventInteraction() {
+
+    }
+
+    @Override
+    public void onCancelEventInteraction() {
+
+    }
+
+    @Override
+    public void onAddEventInteraction() {
+
+    }
+
+
+    @Override
+    public void onEditEventInteraction() {
+
     }
 }
