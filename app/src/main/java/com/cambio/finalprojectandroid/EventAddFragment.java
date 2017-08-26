1b060a52f967433ec3b6afa00d0245b04ac2e9ee
chen <chen470@gmail.com>
2017-08-27 00:24:22 +0300

    create access to open the galery

--- app/src/main/java/com/cambio/finalprojectandroid/EventAddFragment.java
+++ app/src/main/java/com/cambio/finalprojectandroid/EventAddFragment.java
@@ -4,7 +4,10 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.cambio.finalprojectandroid.model.Event;
 import com.cambio.finalprojectandroid.model.Model;
@@ -21,6 +25,10 @@ import com.cambio.finalprojectandroid.utils.Date;
 import com.cambio.finalprojectandroid.utils.Time;
 import com.cambio.finalprojectandroid.widget.MyDatePicker;
 import com.cambio.finalprojectandroid.widget.MyTimePicker;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import static android.app.Activity.RESULT_OK;
 import static android.view.View.GONE;
@@ -33,7 +41,7 @@ public class EventAddFragment extends Fragment {
     ImageView imageView;
     Bitmap imageBitmap;
     ProgressBar progressBar;
-
+    public static final int IMAGE_GALLERY_REQUEST = 20;
     public EventAddFragment() {
         // Required empty public constructor
     }
@@ -149,18 +157,65 @@ public class EventAddFragment extends Fragment {
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+       /* Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+        }*/
+        /*   Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }*/
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+
+        // where do we want to find the data?
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String pictureDirectoryPath = pictureDirectory.getPath();
+        // finally, get a URI representation
+        Uri data = Uri.parse(pictureDirectoryPath);
+
+        // set the data and type.  Get all image types.
+        photoPickerIntent.setDataAndType(data, "image/*");
+
+        // we will invoke this activity, and get something back from it.
+        startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        /*if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             imageBitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(imageBitmap);
+        }*/
+        if (resultCode == RESULT_OK) {
+            // if we are here, everything processed successfully.
+            if (requestCode == IMAGE_GALLERY_REQUEST) {
+                // if we are here, we are hearing back from the image gallery.
+
+                // the address of the image on the SD Card.
+                Uri imageUri = data.getData();
+
+                // declare a stream to read the image data from the SD Card.
+                InputStream inputStream;
+
+                // we are getting an input stream, based on the URI of the image.
+                try {
+                    inputStream = getActivity().getContentResolver().openInputStream(imageUri);
+
+                    // get a bitmap from the stream.
+                    imageBitmap = BitmapFactory.decodeStream(inputStream);
+
+
+                    // show the image to the user
+                    imageView.setImageBitmap(imageBitmap);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    // show a message to the user indictating that the image is unavailable.
+                    Toast.makeText(getActivity(), "Unable to open image", Toast.LENGTH_LONG).show();
+                }
+
+            }
         }
     }
 }
